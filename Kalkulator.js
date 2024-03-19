2074d2267ecf9be3cd2424048ed013b251d5e71d
Bramastio Bagus Aji Wijaya <152799278+Bramastio447@users.noreply.github.com>
2024-03-19 23:21:40 +0700

    V1.0
    File Kalkulator

---
+++ Kalkulator.js
@@ -0,0 +1,21 @@
+const angka1 = document.getElementById('angka1');
+const operasi = document.getElementById('operasi');
+const angka2 = document.getElementById('angka2');
+const hitung = document.getElementById('hitung');
+const hasil = document.getElementById('hasil');
+
+hitung.addEventListener('click', function() {
+    const nilai1 = parseFloat(angka1.value);
+    const nilai2 = parseFloat(angka2.value);
+    const operasiPilihan = operasi.value;
+
+    let hasilPerhitungan;
+
+    switch (operasiPilihan) {
+        case '+':
+            hasilPerhitungan = nilai1 + nilai2;
+            break;
+    }
+
+    hasil.textContent = hasilPerhitungan;
+});
